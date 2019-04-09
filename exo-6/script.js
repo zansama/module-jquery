eec26eda6033c07835335322136762577a6a5a27
zan <clement.mortas@le-campus-numerique.fr>
2019-04-09 15:44:56 +0200

    exo 6

--- exo-6/script.js
+++ exo-6/script.js
@@ -1,5 +1,33 @@
 console.log("exercice 6");
 $(document).ready(function () {
-    $('button[title="se_connecter"]').hide();
+    $('button[title="se_connecter"]').on('click', event => {
+        let email = $('#email');
+        let password = $('#passwword');
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!$(email).val()) {
+            alert('veuillez renseigner un email');
+            $('#email').addClass('alert alert-danger');
+            return false
+        } else if (!regex.test(email.val())) {
+            alert(email.val() + ' n\'est pas un email valide')
+            return false
+        }
+        if (!$(password).val()) {
+            alert('veuillez renseigner un mot de passe');
+            $('#passwword').addClass('alert alert-danger');
+            return false
+        } else if (password.val().length < 6) {
+            alert('mot de passe trop court');
+            return false
+        }
+        $('#passwword').removeClass('alert alert-danger');
+        $('#email').removeClass('alert alert-danger');
+        alert('Vous êtes connecté');
+
+    })
+
+    function isEmail(email) {
+        return regex.test(email);
+    }
 
 });
